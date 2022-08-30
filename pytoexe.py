03a1e8de998350d3a20e7f9db62fd96e404dc376
Shakal1105 <55797859+Shakal1105@users.noreply.github.com>
2022-08-31 00:26:50 +0300

    Update pytoexe.py

--- pytoexe.py
+++ pytoexe.py
@@ -2,7 +2,7 @@ from cx_Freeze import setup, Executable
 import sys, tkinter.filedialog, tkinter
 
 root = tkinter.Tk()
-def setuper(base, filecompile):
+def setuper(base):
     filecompile = tkinter.filedialog.askopenfilename(title="Файл которий в exe")
     root.destroy()
     setup(
@@ -18,10 +18,10 @@ x32_x64 = sys.platform
 
 if x32_x64 == 'win32':
     base = "Win32GUI"
-    setuper(base=base, filecompile=filecompile)
+    setuper(base=base)
 elif x32_x64 == "win64":
     base = "Win64GUI"
-    setuper(base=base, filecompile=filecompile)
+    setuper(base=base)
 else:
     print("Error, this compile only x32 or x64 Windows")
 
